d23694d0f27fedeeb8789ce458f74acccee03787
winwto <liuxuenbrw@163.com>
2019-11-22 15:05:18 +0800

    rabbit连接配置

---
+++ src/main/java/com/lx/constants/ConnectionsMessage.java
@@ -0,0 +1,12 @@
+package com.lx.constants;
+
+public class ConnectionsMessage {
+    // rabbit Ip（使用默认端口）
+    public static final String HOST = "xx.xx.xx.xx";
+
+    // rabbit 登录名（默认guest）
+    public static final String USER = "user";
+
+    // rabbit 登录密码（默认guest）
+    public static final String PASSWORD = "password";
+}
